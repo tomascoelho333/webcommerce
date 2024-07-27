a440d5ee2197340deea94d7047ac2b1cd4c2eb4f
tomascoelho333 <114479795+tomascoelho333@users.noreply.github.com>
2024-07-27 18:35:54 +0100

    Models e Controllers criados.

---
+++ leiricar/backend/controllers/CategoriasController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backend\controllers;
+
+class CategoriasController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
